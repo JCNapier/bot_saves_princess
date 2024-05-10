140797cd67901feabdb8a3d9a1c6fa87728992da
John Napier <jcee.napier@gmail.com>
2024-05-10 16:45:17 -0600

    Add GamePlay spec for runner file capabilities

---
+++ bot_1/bot_saves_princess_runner.rb
@@ -0,0 +1,12 @@
+# require './lib/grid'
+# require './lib/bot_saves_princess'
+
+# puts "Welcome to Bot Saves Princess" 
+# sleep(2)
+# puts "Please enter an odd number between 3 and 100 to determine the grid size"
+# size = gets.chomp.to_i
+
+# until (size.odd?) && (size >= 3 && size < 100) do 
+#   puts "Please try again"
+#   size = gets.chomp.to_i
+# end

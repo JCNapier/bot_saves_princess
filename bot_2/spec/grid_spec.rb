915a078f1d1dc4a5200e6a42e4342db918ea9f22
John Napier <jcee.napier@gmail.com>
2024-05-10 20:57:30 -0600

    Add Grid spec file with tests

---
+++ bot_2/spec/grid_spec.rb
@@ -0,0 +1,46 @@
+require './lib/grid' 
+
+RSpec.describe do 
+  context 'initialize' do
+    let(:grid) {Grid.new(5, 2, 3)}
+    
+    it 'exists' do 
+      expect(grid).to be_a(Grid)
+    end
+
+    it 'has a size attribute' do 
+      expect(grid.size).to eq(5)
+    end
+
+    it 'has a row attribute' do 
+      expect(grid.row).to eq(2)
+    end
+
+    it 'has a row attribute' do 
+      expect(grid.row).to eq(3)
+    end
+  end
+
+  context 'build_grid' do 
+    let(:grid_1) {Grid.new(5, 2, 3)}
+    let(:grid_2) {Grid.new(7, 2, 3)}
+
+    it 'can build a 5x5 grid' do 
+      expect(grid_1.build_grid).to eq(["-----", "-----","-----", "-----", "-----"])
+    end
+
+    it 'can build a 7x7 grid' do
+      expect(grid_2.build_grid).to eq(["-----", "-----","-----", "-----", "-----", "-----", "-----"])
+    end
+  end
+
+  context 'place_bot' do 
+    let(:grid) {Grid.new(5, 2, 3)}
+
+    it 'can place the bot' do 
+      new_grid = grid.build_grid
+
+      expect(grid.place_bot(new_grid)).to eq(["-----", "-----","---m-", "-----", "-----", "-----", "-----"])
+    end
+  end
+end

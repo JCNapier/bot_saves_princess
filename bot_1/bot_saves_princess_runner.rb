13236be0e8b747a1134415aa5065ba7112a20607
John Napier <jcee.napier@gmail.com>
2024-05-10 18:00:11 -0600

    Add runner file & Gamepley logic/updated tests

--- bot_1/bot_saves_princess_runner.rb
+++ bot_1/bot_saves_princess_runner.rb
@@ -1,12 +1,10 @@
-# require './lib/grid'
-# require './lib/bot_saves_princess'
+require './lib/grid'
+require './lib/bot_saves_princess'
+require './lib/game_play'
 
-# puts "Welcome to Bot Saves Princess" 
-# sleep(2)
-# puts "Please enter an odd number between 3 and 100 to determine the grid size"
-# size = gets.chomp.to_i
+def start
+  new_game = GamePlay.new
+  new_game.play
+end
 
-# until (size.odd?) && (size >= 3 && size < 100) do 
-#   puts "Please try again"
-#   size = gets.chomp.to_i
-# end
+start
---
+++ bot_1/spec/game_play_spec.rb
@@ -0,0 +1,44 @@
+require './lib/game_play' 
+
+RSpec.describe GamePlay do
+  context 'build_grid' do
+    let(:game) {GamePlay.new}
+
+    it 'can build a 3x3 grid' do
+      expect(game.build_grid(3)).to eq(["---", "---", "---"])
+    end
+
+    it 'can build a 5x5 grid' do
+      expect(game.build_grid(5)).to eq(["-----", "-----", "-----", "-----", "-----"])
+    end
+  end
+
+  context 'place_bot_in_center' do
+    let(:game) {GamePlay.new}
+
+    it 'can place the bot in the center 3x3' do
+      expect(game.place_bot_in_center(["---", "---", "---"], 3)).to eq(["---", "-m-", "---"])
+    end
+
+    it 'can place the bot in the center 5x5' do
+      expect(game.place_bot_in_center(["-----", "-----", "-----", "-----", "-----"], 5)).to eq(["-----", "-----", "--m--", "-----", "-----"])
+    end
+  end
+
+  context 'place_princess' do
+    let(:game) {GamePlay.new}
+
+    it 'can place the princess in the top left corner' do
+      expect(game.place_princess(["---", "---", "---"], "tl")).to eq(["p--", "---", "---"])
+    end
+    it 'can place the princess in the top right corner' do
+      expect(game.place_princess(["---", "---", "---"], "tr")).to eq(["--p", "---", "---"])
+    end
+    it 'can place the princess in the bottom left corner' do
+      expect(game.place_princess(["---", "---", "---"], "bl")).to eq(["---", "---", "p--"])
+    end
+    it 'can place the princess in the bottom right corner' do
+      expect(game.place_princess(["---", "---", "---"], "br")).to eq(["---", "---", "--p"])
+    end
+  end
+end
